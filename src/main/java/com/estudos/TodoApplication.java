ea2cddb873e3635cc0459e7b2249ed60cad89888
Beatriz-CGomes <beatriz.cg15@hotmail.com>
2025-04-01 21:48:34 -0300

    Ajustes

--- src/main/java/com/estudos/TodoApplication.java
+++ src/main/java/com/estudos/TodoApplication.java
@@ -1,34 +1,13 @@
 package com.estudos;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.estudos.domain.Todo;
-import com.estudos.repositories.TodoRepository;
-
 @SpringBootApplication
-public class TodoApplication implements CommandLineRunner {
-
-	@Autowired
-	private TodoRepository todoRepository;
+public class TodoApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-		Todo t1 = new Todo(null, "Estudar", "Estudar Spring 2 com Angular",
-				LocalDateTime.parse("24/03/2025 20:28", formatter), false);
-
-		todoRepository.saveAll(Arrays.asList(t1));
 	}
 
 }
