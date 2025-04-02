4cd7c548cfe638dd49a5727f0028a135d0193c5e
Beatriz-CGomes <beatriz.cg15@hotmail.com>
2025-04-01 21:30:34 -0300

    Alteracao no Data e Formato da Data

--- src/main/java/com/estudos/services/DBService.java
+++ src/main/java/com/estudos/services/DBService.java
@@ -1,5 +1,7 @@
 package com.estudos.services;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
@@ -16,19 +18,16 @@ public class DBService {
 	@Autowired
 	private TodoRepository todoRepository;
 
-	public void instanciaBaseDados() {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-		Todo t1 = new Todo(null, "Estudar", "Estudar Spring 2 com Angular",
-				LocalDateTime.parse("24/03/2025 20:28", formatter), false);
-		
-		Todo t2 = new Todo(null, "Correr", "Meia Maratona do Rio",
-				LocalDateTime.parse("30/03/2025 20:28", formatter), true);
-		
-		Todo t3 = new Todo(null, "Estudar", "JavaScript",
-				LocalDateTime.parse("27/02/2025 20:28", formatter), false);
-		
-		Todo t4 = new Todo(null, "Estudar", "Para Concurso",
-				LocalDateTime.parse("24/01/2025 20:28", formatter), true);
+	public void instanciaBaseDados() throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		Todo t1 = new Todo(null, "Estudar", "Estudar Spring 2 com Angular", sdf.parse("25/03/2025"), false);
+
+		Todo t2 = new Todo(null, "Correr", "Meia Maratona do Rio", sdf.parse("30/03/2025"), true);
+
+		Todo t3 = new Todo(null, "Estudar", "JavaScript", sdf.parse("27/02/2025"), false);
+
+		Todo t4 = new Todo(null, "Estudar", "Para Concurso", sdf.parse("24/01/2025"), true);
 
 		todoRepository.saveAll(Arrays.asList(t1, t2, t3, t4));
 	}
